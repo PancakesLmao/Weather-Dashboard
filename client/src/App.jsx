8b3a4a83a76f60b03ab1034fc741ef80378ced0e
PancakesLmao <phucthin29@gmail.com>
2024-03-17 02:10:58 +0700

    fixed sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import Sidebar from "./components/Sidebar";
+import Content from "./components/Content";
 import "./App.css";
 
 function App() {
   return (
     <>
       <div>
-        
+        <main>
+          <Sidebar />
+          <Content />
+        </main>
       </div>
     </>
   );
