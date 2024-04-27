e9d51d0d9c896c92c43a5ef2787c77184d17ef6d
PancakesLmao <phucthin29@gmail.com>
2024-04-28 01:44:55 +0700

    remove import react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
