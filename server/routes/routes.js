a6003ad63b5b8a22d1495a554611378bddafcdae
MinhNguyen312 <nhatminhnguyenquang312@gmail.com>
2024-04-28 14:02:38 +0700

    [Feat] Dynamically display wind and rainfall chart

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -28,4 +28,85 @@ router.get('/getAll', async (req,res) => {
     
 })
 
+//  Get Wind Chart Data
+router.get('/getWindChartData', async (req,res) => {
+
+    try{
+        const data = await Model.aggregate([
+            {
+                $group: {
+                  _id: "$recordDate",
+                  totalWindSpeed: { $sum: "$avgWindSpd" },
+                  maxWindSpeed:{$max:"$mxWindSpd"},
+                  count: { $sum: 1 } // Count the number of documents in each group
+                }
+              },
+              {
+                $project: {
+                  _id: 1, 
+                  avgWindSpeed: { $divide: ["$totalWindSpeed", "$count"] }, // Calculate average wind speed
+                  maxWindSpeed: "$maxWindSpeed"
+                }
+              }
+        ])
+
+        res.json(data);
+    } 
+    catch (err){
+        console.error("Error retrieving daily average wind speed", err);
+        res.status(500).send("Error retrieving data from MongoDB");
+    }
+
+})
+
+//  Get Rain Chart Data
+router.get('/getRainChartData', async (req,res) => {
+
+    try{
+        const data = await Model.aggregate([
+            {
+                $group: {
+                  _id: {$month: {$toDate: "$recordDate"}}, //Extract month from recordDate
+                  totalRainPerDay: { $sum: "$rainPerDay" },
+                  maxRainPerDay: {$max: "$rainPerDay"},
+                  count: { $sum: 1 } // Count the number of documents in each group
+                }
+              },
+              {
+                $addFields: {
+                    monthName: {
+                        $arrayElemAt:[
+                            [
+                                "January", "February", "March", "April", "May", "June",
+                            "July", "August", "September", "October", "November", "December"
+                            ],{$subtract: ["$_id", 1]}
+                        ] 
+                    }
+                }
+              },
+              {
+            
+                $sort: {
+                  _id: 1 // Sort by _id in ascending order
+                }
+          },
+              {
+                $project: {
+                  _id: "$monthName", 
+                  avgRainPerMonth: { $divide: ["$totalRainPerDay", "$count"] }, // Calculate average wind speed
+                  maxRainPerMonth: "$maxRainPerDay"
+                }
+              },
+              
+        ])
+
+        res.json(data);
+    } 
+    catch (err){
+        console.error("Error retrieving daily average wind speed", err);
+        res.status(500).send("Error retrieving data from MongoDB");
+    }
+
+})
+
 module.exports = router;
